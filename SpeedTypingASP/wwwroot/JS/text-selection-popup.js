f99d338f8ad7352f40774d897d59c4174eb0a202
ZTMatvey <73705821+ZTMatvey@users.noreply.github.com>
2021-09-14 20:59:30 +0700

    Начал добавлять настройку размера текста при его выборе

---
+++ SpeedTypingASP/wwwroot/JS/text-selection-popup.js
@@ -0,0 +1,27 @@
+﻿let tsPopupElem = document.getElementById('text-selection__popup');
+let blurSreenElem = document.getElementById('blur-screen');
+let popupTextTitleElem = document.getElementById('text-selection__popup-text-title');
+
+ClosePopUp(true);
+
+function OpenPopUp(textName) {
+    if (tsPopupElem.classList.contains('center-disactive'))
+        tsPopupElem.classList.remove('center-disactive');
+    tsPopupElem.classList.add('center-active');
+
+    if (blurSreenElem.classList.contains('blur-screen-disactive'))
+        blurSreenElem.classList.remove('blur-screen-disactive');
+    blurSreenElem.classList.add('blur-screen-active');
+
+    popupTextTitleElem.innerHTML = textName;
+}
+function ClosePopUp(isOnLoad) {
+    if (!isOnLoad) {
+        blurSreenElem.classList.add('blur-screen-disactive');
+        tsPopupElem.classList.add('center-disactive');
+    }
+    if (tsPopupElem.classList.contains('center-active'))
+        tsPopupElem.classList.remove('center-active');
+    if (blurSreenElem.classList.contains('blur-screen-active'))
+        blurSreenElem.classList.remove('blur-screen-active');
+}
---
+++ SpeedTypingASP/wwwroot/JS/text-selection-slider.js
@@ -0,0 +1,30 @@
+﻿let popupCharacterCountElem = document.getElementById('text-selection__popup-character-count');
+let popupSliderElem = document.getElementById('text-selection__popup-slider');
+setSliderValue('3');
+
+function setSliderValue(value) {
+    popupSliderElem.value = value;
+    UpdateSize(value);
+}
+
+function UpdateSize(value) {
+    let textSizeStr = 'Размер текста: ';
+    switch (value) {
+        case '1':
+            textSizeStr += 'extremely short (50 символов)';
+            break;
+        case '2':
+            textSizeStr += 'short (25%)';
+            break;
+        case '3':
+            textSizeStr += 'medium (50%)';
+            break;
+        case '4':
+            textSizeStr += 'large (75%)';
+            break;
+        case '5':
+            textSizeStr += 'full (100%)';
+            break;
+    }
+    popupCharacterCountElem.innerHTML = textSizeStr;
+}

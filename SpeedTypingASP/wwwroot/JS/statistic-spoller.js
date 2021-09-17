ca4271e783705f2ad40312d42b1418a0988d1003
ZTMatvey <73705821+ZTMatvey@users.noreply.github.com>
2021-09-17 19:49:04 +0700

    Добавлена возможность сворачивать статистику отдельных текстов

---
+++ SpeedTypingASP/wwwroot/JS/statistic-spoller.js
@@ -0,0 +1,9 @@
+﻿$('.personal-account__text-name')
+    .toggleClass('personal-account__text-name-disactive')
+    .parent().next().slideToggle(300);
+
+$('.personal-account__text-name').click(function (e) {
+    $(this)
+        .toggleClass('personal-account__text-name-active')
+        .toggleClass('personal-account__text-name-disactive').parent().next().slideToggle(300);
+});
